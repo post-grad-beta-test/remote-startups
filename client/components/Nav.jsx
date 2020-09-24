283fa015ae0b4c71dbb792a38e66dbb29d6849ae
aidan-starke <starke.aidan@gmail.com>
2020-09-25 11:21:00 +1200

    refactor, add conditional rendering

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,11 +1,40 @@
-import { Anchor, Box, Header, Menu, Nav, ResponsiveContext } from 'grommet'
 import React from 'react'
+import { connect } from 'react-redux'
+import { isAuthenticated } from 'authenticare/client'
+import { Anchor, Box, Header, Menu, Nav, ResponsiveContext } from 'grommet'
+import { logOff } from 'authenticare/client/auth'
+import { changePage } from '../actions'
 
-function NavHeader () {
-  return (
-    <Header background="dark-1" pad="medium">
-      <Box direction="row" align="center" gap="small">
+function NavHeader({ dispatch }) {
+  if (isAuthenticated())
+    return (
+      <Header background="dark-1" pad="medium">
+        <Box direction="row" align="center" gap="small" onClick={() => dispatch(changePage('Home'))} >
           Coject - Yourself, With Others
+      </Box>
+        <ResponsiveContext.Consumer>
+          {responsive =>
+            responsive === 'small' ? (
+              <Menu
+                label="Go Places"
+                items={[
+                  { label: 'Log Off', onClick: () => logOff() }
+                ]}
+              />
+            ) : (
+                <Nav direction="row">
+                  <Anchor onClick={() => logOff()} label="Log Off" />
+                </Nav>
+              )
+          }
+        </ResponsiveContext.Consumer>
+      </Header>
+    )
+
+  else return (
+    <Header background="dark-1" pad="medium">
+      <Box direction="row" align="center" gap="small" onClick={() => dispatch(changePage('Home'))} >
+        Coject - Yourself, With Others
       </Box>
       <ResponsiveContext.Consumer>
         {responsive =>
@@ -13,21 +42,20 @@ function NavHeader () {
             <Menu
               label="Go Places"
               items={[
-                { label: 'Log In', onClick: () => {} },
-                { label: 'Register', onClick: () => {} }
+                { label: 'Log In', onClick: () => dispatch(changePage('Login')) },
+                { label: 'Register', onClick: () => dispatch(changePage('Register')) }
               ]}
             />
           ) : (
-            <Nav direction="row">
-              <Anchor href="#" label="Log In" />
-              <Anchor href="#" label="Register" />
-            </Nav>
-          )
+              <Nav direction="row">
+                <Anchor onClick={() => dispatch(changePage('Login'))} label="Log In" />
+                <Anchor onClick={() => dispatch(changePage('Register'))} label="Register" />
+              </Nav>
+            )
         }
       </ResponsiveContext.Consumer>
     </Header>
-
   )
 }
 
-export default NavHeader
+export default connect()(NavHeader)
