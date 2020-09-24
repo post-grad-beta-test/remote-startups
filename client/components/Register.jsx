936a31808288bf85fc78005d4f736ece7bb0760c
Emma Slight <emma.slight@icloud.com>
2020-09-25 11:51:01 +1200

    get authentication working

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable promise/always-return */
 import { yupResolver } from '@hookform/resolvers'
 import { isAuthenticated, register } from 'authenticare/client'
 import { Button, Form, FormField, TextInput } from 'grommet'
@@ -13,7 +14,7 @@ const schema = yup.object().shape({
   email: yup
     .string()
     .email()
-    .required('Email is a required field'),
+    .required('Email is a required field')
   password: yup
     .string()
     .required('Please enter your password')
@@ -27,7 +28,6 @@ const schema = yup.object().shape({
     .when('password', {
       is: password => (!!(password && password.length > 0)),
       then: yup.string().oneOf([yup.ref('password')], "Password doesn't match")
-    })
 })
 
 function Register () {
@@ -35,8 +35,8 @@ function Register () {
   const { handleSubmit, control, errors } = useForm({
     resolver: yupResolver(schema) }
   )
-  const onSubmit = ({ values }) => {
-    const { username, password } = values
+  const onSubmit = (values) => {
+    const { username, password, email } = values
     register({ username, password }, { baseUrl })
       .then((token) => {
         if (isAuthenticated()) {
@@ -53,10 +53,11 @@ function Register () {
       })
       .catch(err => alert(err.message))
   }
-
-  console.log(values)
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormField label='username' name='username'>
+        <Controller as={TextInput} name="username" control={control} defaultValue=""/>
+      </FormField>
       <FormField label='email' name='email'>
         <Controller as={TextInput} name="email" control={control} defaultValue=""/>
         <p>{errors.email?.message}</p>
@@ -75,51 +76,3 @@ function Register () {
 }
 export default Register
 
-// function Register ({ dispatch }) {
-//   const [newUser] = useState({})
-//   const [confirmPassword, setConfirmPassword] = useState('')
-
-//   const emailHandler = event => {
-//     event.preventDefault()
-//     newUser.username = event.target.value
-//   }
-
-//   const passwordHandler = event => {
-//     event.preventDefault()
-//     newUser.password = event.target.value
-//   }
-
-//   const confirmPasswordHandler = event => {
-//     event.preventDefault()
-//     setConfirmPassword(event.target.value)
-//   }
-
-//   const submitHandler = (event) => {
-//     event.preventDefault()
-//     const { username, password } = newUser
-
-//     if (password === confirmPassword) {
-
-//   }
-
-//   return (
-//     <Box width='medium' elevation='medium' pad='medium' >
-//       <Text size='medium'>Register</Text>
-//       <Form onSubmit={submitHandler}>
-//         <FormField label='email' name='email'>
-//           <TextInput name='email' type='email' onChange={emailHandler} placeholder='example@mail.com' />
-//         </FormField>
-//         <FormField label='password' name='password'>
-//           <TextInput name='password' type='password' onChange={passwordHandler} />
-//         </FormField>
-//         <FormField label="confirm-password" name='confirm-password'>
-//           <TextInput name='confirmPassword' type='password' onChange={confirmPasswordHandler} />
-//         </FormField>
-
-//         <Button type='submit' value='Submit' label='Submit' />
-//       </Form>
-//     </Box>
-//   )
-// }
-
-// export default connect()(Register)
