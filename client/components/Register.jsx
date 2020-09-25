a2f25bbdca4f52fbd8ef7eb1d69c4a41194b40ad
Emma Slight <emma.slight@icloud.com>
2020-09-25 13:39:59 +1200

    add box to form

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable promise/always-return */
 import { yupResolver } from '@hookform/resolvers'
 import { isAuthenticated, register } from 'authenticare/client'
-import { Button, Form, FormField, TextInput } from 'grommet'
+import { Box, Button, Form, FormField, TextInput } from 'grommet'
 import React from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
@@ -56,30 +56,32 @@ function Register () {
       .catch(err => alert(err.message))
   }
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormField label='First name' name='firstName'>
-        <Controller as={TextInput} name="firstName" control={control} defaultValue=""/>
-      </FormField>
-      <FormField label='Last name' name='lastName'>
-        <Controller as={TextInput} name="lastName" control={control} defaultValue=""/>
-      </FormField>
-      <FormField label='username' name='username'>
-        <Controller as={TextInput} name="username" control={control} defaultValue=""/>
-      </FormField>
-      <FormField label='email' name='email'>
-        <Controller as={TextInput} name="email" control={control} defaultValue="" />
-        <p>{errors.email?.message}</p>
-      </FormField>
-      <FormField label='password' name='password'>
-        <Controller as={TextInput} name="password" type="password" control={control} defaultValue="" />
-        <p>{errors.password?.message}</p>
-      </FormField>
-      <FormField label="confirm-password" name='confirm-password'>
-        <Controller as={TextInput} name="confirmPassword" type="password" control={control} defaultValue="" />
-        <p>{errors.confirmPassword?.message}</p>
-      </FormField>
-      <Button type='submit' value='Submit' label='Submit' />
-    </Form>
+    <Box align='center' pad='large' >
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormField label='First name' name='firstName'>
+          <Controller as={TextInput} name="firstName" control={control} defaultValue=""/>
+        </FormField>
+        <FormField label='Last name' name='lastName'>
+          <Controller as={TextInput} name="lastName" control={control} defaultValue=""/>
+        </FormField>
+        <FormField label='username' name='username'>
+          <Controller as={TextInput} name="username" control={control} defaultValue=""/>
+        </FormField>
+        <FormField label='email' name='email'>
+          <Controller as={TextInput} name="email" control={control} defaultValue="" />
+          <p>{errors.email?.message}</p>
+        </FormField>
+        <FormField label='password' name='password'>
+          <Controller as={TextInput} name="password" type="password" control={control} defaultValue="" />
+          <p>{errors.password?.message}</p>
+        </FormField>
+        <FormField label="confirm-password" name='confirm-password'>
+          <Controller as={TextInput} name="confirmPassword" type="password" control={control} defaultValue="" />
+          <p>{errors.confirmPassword?.message}</p>
+        </FormField>
+        <Button type='submit' value='Submit' label='Submit' />
+      </Form>
+    </Box>
   )
 }
 export default Register
