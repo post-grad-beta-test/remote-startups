45671817c6ce1a41c4c1c162ba0e168230dac1cb
Emma Slight <emma.slight@icloud.com>
2020-09-27 21:52:36 +1300

    merge tabs into master

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,57 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Home from './Home'
-import Login from './Login'
 import Nav from './Nav'
 import Register from './Register'
-import UserTabs from './UserTabs'
-const theme = {
-  global: {
-    font: {
-      family: 'Raleway',
-      size: '18px',
-      height: '20px'
-    },
-    colors: {
-      blue: '#00C8FF',
-      green: '#17EBA0',
-      teal: '#82FFF2',
-      purple: '#F740FF',
-      red: '#FC6161',
-      orange: '#FFBC44',
-      yellow: '#FFEB59'
-    }
-  },
-  card: {
-    footer: {
-      pad: { horizontal: 'medium', vertical: 'small' },
-      background: '#FFFFFF27'
-    }
-  }
-}
 
 const App = ({ currentPage }) => {
   return (
-    <Grommet theme={theme}>
-      <NavHeader />
-      <UserTabs />
-      <Home />
-      <Box Box fill align="center" justify="center">
-        <Box>
-          <WorldMap color="#173F5F" opacity="0.8" />
-        </Box>
-        <Box pad="medium">
-          <Heading size="large">Co-ject!</Heading>
-          <Text size="xlarge" color='accent-5'>An online collaboration space</Text>
-        </Box>
-        <Register />
-        {/* {currentPage === 'Home' && <div>Success! </div>} */}
-      </Box>
-    </Grommet>
+    <>
+      <Nav />
+      {currentPage === 'Home' && <Home />}
+      {currentPage === 'Login' && <Login />}
+      {currentPage === 'Register' && <Register />}
+    </>
   )
 }
-
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   return {
     currentPage: state.currentPage
   }
