c1c2e70c1069e30512185ccafbbb7157af110334
aidan-starke <starke.aidan@gmail.com>
2020-09-28 10:45:48 +1300

    fix rendering user info

--- client/components/UserTabs.jsx
+++ client/components/UserTabs.jsx
@@ -21,16 +21,16 @@ function UserTabs({ dispatch, user }) {
               <Image src="https://picsum.photos/200" />
             </FormField>
             <FormField label="email">
-              <TextInput placeholder="john@gmail.com" />
+              <TextInput placeholder={user.email} />
             </FormField>
             <FormField label="Username">
-              <TextInput />
+              <TextInput placeholder={user.username} />
             </FormField>
             <FormField label="First Name">
-              <TextInput />
+              <TextInput placeholder={user.first_name} />
             </FormField>
             <FormField label="Last Name">
-              <TextInput />
+              <TextInput placeholder={user.last_name} />
             </FormField>
             <FormField label="My Interests">
               <Select options={["ACCOUNTING", "AGRICULTURAL SCIENCE", "APPLIED SCIENCES", "ARCHITECTURE", "BANKING & FINANCE", "BIOCHEMISTRY", "BUSINESS ADMINISTRATION & MANAGEMENT", "CHEMISTRY", "CHEMICAL ENGINEERING", "CIVIL ENGINEERING", "COMMUNITY", "COMMUNICATION", "COMPUTER ENGINEERING", "COMPUTER SCIENCE", "CRIMINOLOGY", "ECONOMICS", "EDUCATION", "ELECTRICAL & ELECTRONICS ENGINEERING", "ENGINEERING", "ENGLISH LANGUAGE & LITERATURE", "ENTREPRENEURSHIP", "ENVIRONMENTAL DESIGN", "ENVIRONMENTAL SCIENCE", "ESTATE MANAGEMENT", "FRENCH", "GARDENING", "GAMING", "GEOLOGY", "HISTORY", "HUMAN RESOURCE MANAGEMENT", "INSURANCE", "INTERNATIONAL RELATIONS", "LAW", "LIBRARY SCIENCE", "LINGUISTICS & COMMUNICATION", "MARKETING", "MASS COMMUNICATION", "MATHEMATICS", "MECHANICAL ENGINEERING", "MEDICAL & HEALTH SCIENCE", "MICROBIOLOGY", "NURSING", "OFFICE TECHNOLOGY MANAGEMENT", "PERMACULTURE", "PHARMACEUTICAL SCIENCES", "PHILOSOPHY", "PHYSICS", "POLITICAL SCIENCE", "PROJECT MANAGEMENT", "PSYCHOLOGY", "PUBLIC ADMINISTRATION", "PUBLIC HEALTH", "PURCHASING & SUPPLY CHAIN MANAGEMENT", "QUANTITY SURVEY", "SCIENCE LABORATORY TECHNOLOGY", "SOCIOLOGY", "STATISTICS", "SUSTAINABILITY", "TECHNOLOGY", "THEATRE ARTS", "THEOLOGY & BIBLICAL STUDIES", "URBAN & REGIONAL PLANNING", "VETERINARY MEDICINE"]} multiple searchPlaceholder="Search Interests" />
