aaf491335c8f7eb359e5246423924e87c620a66a
aidan-starke <starke.aidan@gmail.com>
2020-09-29 15:46:30 +1300

    refactor

---
+++ client/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+    global: {
+        font: {
+            family: 'Raleway',
+            size: '18px',
+            height: '20px'
+        },
+        colors: {
+            blue: '#00C8FF',
+            green: '#17EBA0',
+            teal: '#82FFF2',
+            purple: '#F740FF',
+            red: '#FC6161',
+            orange: '#FFBC44',
+            yellow: '#FFEB59'
+        }
+    },
+    card: {
+        footer: {
+            pad: { horizontal: 'medium', vertical: 'small' },
+            background: '#FFFFFF27'
+        }
+    }
+}
+
+module.exports = theme
