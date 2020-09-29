67e5cadf489560e7776872ba907ce04c5770d554
aidan-starke <starke.aidan@gmail.com>
2020-09-30 09:50:49 +1300

    edit seeds

--- seeds/1-projects.js
+++ seeds/1-projects.js
@@ -4,10 +4,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('projects').insert([
-        { id: 1, user_id: 2, name: 'Remote Startups', description: 'Tech startups from home!', date_start: 20 / 12 / 10, date_end: 20 / 12 / 14 },
-        { id: 2, user_id: 3, name: 'Test one', description: 'this is an event', date_start: 20 / 11 / 20, date_end: 20 / 11 / 24 },
-        { id: 3, user_id: 2, name: 'test Two', description: 'this is another event', date_start: 20 / 11 / 13, date_end: 20 / 11 / 18 }
-
+        { id: 1, user_id: 1, name: 'Remote Startups', description: 'Tech startups from home!', date_start: '23/09/20', date_end: '30/09/20' },
+        { id: 2, user_id: 2, name: 'Tea time', description: 'Shopping list based on selected recipes for tea time', date_start: '23/09/20', date_end: '30/09/20' },
+        { id: 3, user_id: 3, name: 'Financy', description: 'A website built to help making saving fun and interactive', date_start: '23/09/20', date_end: '30/09/20' }
       ])
     })
 }
