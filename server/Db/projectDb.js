c389e79cf872ff4cc577996551353309974b5de3
aidan-starke <starke.aidan@gmail.com>
2020-09-29 15:47:32 +1300

    rename args

--- server/Db/projectDb.js
+++ server/Db/projectDb.js
@@ -2,8 +2,8 @@ const knex = require('knex')
 const config = require('../../knexfile').development
 const connection = knex(config)
 
-function saveNewEvent (id, body, db = connection) {
-  const { eventName, description, startDate, endDate } = body
+function saveNewEvent (id, event, db = connection) {
+  const { eventName, description, startDate, endDate } = event
   return db('projects')
     .insert({ user_id: id, name: eventName, description: description, date_start: startDate, date_end: endDate })
 }
@@ -17,9 +17,9 @@ function deleteEvent (id, db = connection) {
   return db('projects').where({ id }).delete()
 }
 
-function addUserToEvent (id, eventId, db = connection) {
+function addUserToEvent (userId, eventId, db = connection) {
   return db('users_projects')
-    .insert({ project_id: eventId, user_id: id })
+    .insert({ project_id: eventId, user_id: userId })
 }
 
 function getUsersForEvent ({ eventId, db = connection }) {
