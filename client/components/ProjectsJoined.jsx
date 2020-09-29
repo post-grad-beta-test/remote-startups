380f4921c558cbcef617e41843522148da3dd957
Emma <student@emma-matthews-mac-desktop.hub>
2020-09-30 09:35:26 +1300

    Project list layout

--- client/components/ProjectsJoined.jsx
+++ client/components/ProjectsJoined.jsx
@@ -1,13 +1,16 @@
-import { Box, List, Tab } from 'grommet'
+import { Box, Heading, DataTable, Tab } from 'grommet'
 import { Aggregate } from 'grommet-icons'
 import React from 'react'
 
 
 export const ProjectsJoined = () => {
   return (
-    <Tab title="Projects Joined" icon={<Aggregate />}>
-      <Box align="center" justify="center" pad="xlarge">
-        <List data={[{ 'name': 'Sustainability Project', 'Finishing': '05/10/20' }, { 'name': 'Communication Tool', 'Finishing': '11/10/20' }]} primaryKey="name" secondaryKey="finishing" />
+    <Tab title="Projects" icon={<Aggregate />}>
+      <Box fill="vertical" overflow="auto" align="center" flex="grow">
+        <Heading textAlign="center" color="control">
+          Live Projects
+  </Heading>
+        <DataTable columns={[{ "header": "Name", "property": "name", "primary": true }, { "header": "Owner", "property": "owner" }]} data={[{ "name": "Sustainable Leadership" }, { "name": "Trains" }]} size="medium" onClickRow={[{ "screen": 1, "label": "Screen", "key": 1 }]} pad="medium" />
       </Box>
     </Tab>
   )
