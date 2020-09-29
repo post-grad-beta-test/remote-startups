bee0de4dcc6aae9f0dc3c72e4bdbd911d60ddbda
Emma <student@emma-matthews-mac-desktop.hub>
2020-09-30 09:39:05 +1300

    layout

--- client/components/ProjectsJoined.jsx
+++ client/components/ProjectsJoined.jsx
@@ -10,7 +10,7 @@ export const ProjectsJoined = () => {
         <Heading textAlign="center" color="control">
           Live Projects
   </Heading>
-        <DataTable columns={[{ "header": "Name", "property": "name", "primary": true }, { "header": "Owner", "property": "owner" }]} data={[{ "name": "Sustainable Leadership" }, { "name": "Trains" }]} size="medium" onClickRow={[{ "screen": 1, "label": "Screen", "key": 1 }]} pad="medium" />
+        <DataTable columns={[{ "header": "Name", "property": "name", "primary": true, "sortable": true, "search": true }, { "header": "Dates", "property": "dates", "sortable": true }]} data={[{ "name": "Emma" }, { "name": "Aidan" }]} size="small" onClickRow={[{ "screen": 1, "label": "Screen", "key": 1 }]} pad="medium" />
       </Box>
     </Tab>
   )
