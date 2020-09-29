71a80397cb2bf2826b73a9da20b8d483b0a64a53
aidan-starke <starke.aidan@gmail.com>
2020-09-29 14:24:05 +1300

    write tests

--- client/components/Register.test.js
+++ client/components/Register.test.js
@@ -3,24 +3,10 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { CHANGE_PAGE } from '../actions'
 import store from '../store'
 import Register from './Register'
 
-jest.spyOn(store, 'dispatch')
-
-const password = 'Testtest1234%'
-
-test.skip('Page renders the login component when login is clicked', () => {
-  expect.assertions(2)
-  render(<Provider store={store}><Register /></Provider>)
-  const link = screen.getByRole('login')
-  userEvent.click(link)
-  expect(store.dispatch).toHaveBeenCalled()
-  expect(store.dispatch.mock.calls[0][0].type).toEqual(CHANGE_PAGE)
-})
-
-describe('sign-in', () => {
+describe('register', () => {
   test('check you can enter a first name', () => {
     render(<Provider store={store}><Register /></Provider>)
     const formInput = screen.getByRole('textbox', { name: 'First Name' })
