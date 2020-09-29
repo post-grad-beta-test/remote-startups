24252e931e6df85600714d85c895169e9bde1db5
Emma <student@emma-matthews-mac-desktop.hub>
2020-09-30 09:54:59 +1300

    Add description column

--- client/components/ProjectsJoined.jsx
+++ client/components/ProjectsJoined.jsx
@@ -10,7 +10,7 @@ export const ProjectsJoined = () => {
         <Heading textAlign="center" color="control">
           Live Projects
   </Heading>
-        <DataTable columns={[{ "header": "Name", "property": "name", "primary": true, "sortable": true, "search": true }, { "header": "Dates", "property": "dates", "sortable": true }, { "header": "Topic", "property": "topic", "sortable": true }]} data={[{ "name": "Emma" }, { "name": "Aidan" }]} size="small" onClickRow={[{ "screen": 1, "label": "Screen", "key": 1 }]} pad="medium" />
+        <DataTable columns={[{ "header": "Name", "property": "name", "primary": true, "sortable": true, "search": true }, { "header": "Dates", "property": "dates", "sortable": true }, { "header": "Topic", "property": "topic", "sortable": true }, { "header": "Description", "property": "description" }]} data={[{ "name": "Emma" }, { "name": "Aidan" }]} size="small" onClickRow={[{ "screen": 1, "label": "Screen", "key": 1 }]} pad="medium" />
       </Box>
     </Tab>
   )
