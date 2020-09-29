ee316c9ee41016485967515be4b5570e092bf3c7
aidan-starke <starke.aidan@gmail.com>
2020-09-29 16:34:24 +1300

    conditionally render nav using isAuthenticated()

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { isAuthenticated } from 'authenticare/client'
 import LoggedIn from './LoggedIn'
 import LoggedOff from './LoggedOff'
 
-function Nav({ navState }) {
+function Nav() {
     return (
       <>
-        {navState === 'Logged In' && <LoggedIn />}
-        {navState === 'Logged Off' && <LoggedOff />}
+        {isAuthenticated() && <LoggedIn />}
+        {!isAuthenticated() && <LoggedOff />}
       </>
     )
   }
 
-  function mapStateToProps(state) {
-    return {
-      navState: state.navState
-    }
-  }
-
-  export default connect(mapStateToProps)(Nav)
+  export default Nav
