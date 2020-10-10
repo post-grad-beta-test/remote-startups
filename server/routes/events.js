95940b79b916ac40221989b25c8b05b07d0a7c50
Emma Slight <emma.slight@icloud.com>
2020-10-10 21:36:01 +1300

    add routes for Users events

--- server/routes/events.js
+++ server/routes/events.js
@@ -8,6 +8,8 @@ const {
   deleteEvent,
   addUserToEvent,
   getUsersForEvent,
+  getEventsForUser,
+  addUserToEvent,
 } = require('../Db/projectDb')
 
 router.get('/', (req, res) => {
@@ -18,10 +20,50 @@ router.get('/', (req, res) => {
     .catch(() => res.status(500).send('DATABASE ERROR'))
 })
 
+router.get('/eventsUser/:id', getTokenDecoder(), (req, res) => {
+  if (req.user) {
+    const id = Number(req.params.id)
+    getEventsForUser(id)
+      .then((events) => {
+        return res.status(200).json(events)
+      })
+      .catch((err) => console.log(err))
+  } else {
+    res.status(500).send('authentication token not provided')
+  }
+})
+
+router.get('/usersEvent/:id', getTokenDecoder(), (req, res) => {
+  if (req.user) {
+    const id = Number(req.params.id)
+    const { eventId } = req.body
+    getUsersForEvent(id, eventId)
+      .then((users) => {
+        return res.status(200).json(users)
+      })
+      .catch((err) => console.log(err))
+  } else {
+    res.status(500).send('authentication token not provided')
+  }
+})
+
 router.post('/:id', getTokenDecoder(), (req, res) => {
   if (req.user) {
     const id = Number(req.params.id)
     saveNewEvent(id, req.body)
+      .then((ids) => {
+        return res.status(200).json(ids[0])
+      })
+      .catch((err) => console.log(err))
+  } else {
+    res.status(500).send('authentication token not provided')
+  }
+})
+
+router.post('/usersEvent/:id', getTokenDecoder(), (req, res) => {
+  if (req.user) {
+    const id = Number(req.params.id)
+    addUserToEvent(id, req.body)
       .then((ids) => {
         return res.status(200).json(ids[0])
       })
