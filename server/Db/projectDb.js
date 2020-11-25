1b3c9bd9b36a8e268f9547d0d628212d0169391f
Emma Slight <emma.slight@icloud.com>
2020-11-26 12:15:15 +1300

    01 - write db function to get all events for user

--- server/Db/projectDb.js
+++ server/Db/projectDb.js
@@ -2,30 +2,37 @@ const knex = require('knex')
 const config = require('../../knexfile').development
 const connection = knex(config)
 
-function saveNewEvent (id, event, db = connection) {
+function saveNewEvent(id, event, db = connection) {
   const { name, description, startDate, endDate, topic } = event
-  return db('projects')
-    .insert({ user_id: id, name, description, date_start: startDate, date_end: endDate, topic })
+  return db('projects').insert({
+    user_id: id,
+    name,
+    description,
+    date_start: startDate,
+    date_end: endDate,
+    topic,
+  })
 }
 
-function getAllEvents (db = connection) {
-  return db('projects')
-    .select()
+function getAllEvents(db = connection) {
+  return db('projects').select()
 }
 
-function deleteEvent (id, db = connection) {
+function deleteEvent(id, db = connection) {
   return db('projects').where({ id }).delete()
 }
 
-function addUserToEvent (userId, eventId, db = connection) {
-  return db('users_projects')
-    .insert({ project_id: eventId, user_id: userId })
+function addUserToEvent(eventId, userId, db = connection) {
+  return db('users_projects').insert({ project_id: eventId, user_id: userId })
 }
 
-function getUsersForEvent ({ eventId, db = connection }) {
-  return db('users_projects')
-    .where('event_id', eventId)
-    .select('users_id')
+function getUsersForEvent({ eventId, db = connection }) {
+  return db('users_projects').where('project_id', eventId).select('users_id')
+}
+
+function getEventsForUser({ userId, db = connection }) {
+  console.log(userId)
+  return db('users_projects').where('user_id', userId).select('project_id')
 }
 
 module.exports = {
@@ -33,6 +40,6 @@ module.exports = {
   getAllEvents,
   deleteEvent,
   addUserToEvent,
-  getUsersForEvent
-
+  getUsersForEvent,
+  getEventsForUser,
 }
