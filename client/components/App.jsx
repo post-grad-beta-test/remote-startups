7abc4ea62bfcfb9f78bde006a06c4bdc58e305cf
Lex <lexx_stokes@live.com>
2020-11-30 15:42:08 +1300

    init routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
-import { controlNavState } from '../helpers'
-import { Home, Nav, Login, Register, UserTabs } from '../components'
+import React, { useEffect } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import Routes from "./Routes";
+import { connect } from "react-redux";
+import { IfAuthenticated, IfNotAuthenticated } from "./Authenticated";
+import { controlNavState } from "../helpers";
+import { Home, Nav, Login, Register, UserTabs } from "../components";
 
 const App = ({ dispatch, currentPage }) => {
-  useEffect(() => controlNavState(dispatch), [])
+  useEffect(() => controlNavState(dispatch), []);
 
   return (
     <>
-      <Nav />
-      <IfNotAuthenticated>
-        {currentPage === 'Home' && <Home />}
-        {currentPage === 'Login' && <Login />}
-        {currentPage === 'Register' && <Register />}
-      </IfNotAuthenticated>
-      <IfAuthenticated>
-        {currentPage === 'Home' && <Home />}
-        {currentPage === 'User' && <UserTabs />}
-      </IfAuthenticated>
+      <Router>
+        <Routes />
+        <Nav />
+        <IfNotAuthenticated>
+          {currentPage === "Home" && <Home />}
+          {currentPage === "Login" && <Login />}
+          {currentPage === "Register" && <Register />}
+        </IfNotAuthenticated>
+        <IfAuthenticated>
+          {currentPage === "Home" && <Home />}
+          {currentPage === "User" && <UserTabs />}
+        </IfAuthenticated>
+      </Router>
     </>
-  )
-}
+  );
+};
 
 function mapStateToProps(state) {
   return {
-    currentPage: state.currentPage
-  }
+    currentPage: state.currentPage,
+  };
 }
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
