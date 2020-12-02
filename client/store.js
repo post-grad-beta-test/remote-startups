2fa6453ddac3f587ce6c20b257b121a90a4d8056
Emma Slight <emma.slight@icloud.com>
2020-12-02 15:07:12 +1300

    add redux thunk to store with redux tools

--- client/store.js
+++ client/store.js
@@ -1,9 +1,13 @@
-import { createStore } from 'redux'
-
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import reducers from './reducers'
 
-const store = createStore(reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 export default store
