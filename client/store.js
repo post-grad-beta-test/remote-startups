d4426f1a6573493c6aff3bb52ba20ff47413de06
Emma Slight <emma.slight@icloud.com>
2020-12-02 16:24:31 +1300

    add composeWithDevTools to store

--- client/store.js
+++ client/store.js
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from './reducers'
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 
+// The store now has the ability to accept thunk functions in `dispatch`
+const store = createStore(reducers, composedEnhancer)
 export default store
