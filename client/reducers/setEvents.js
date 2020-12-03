b9f25e7ace21ccf4817253f2ad20a671662a4564
Emma Slight <emma.slight@icloud.com>
2020-12-04 01:18:40 +1300

    add JSdoc block to SetEvents reducer

--- client/reducers/setEvents.js
+++ client/reducers/setEvents.js
@@ -1,5 +1,11 @@
 import { SET_EVENTS } from '../actions'
 
+/**
+ * setEvents reducer the adds the setEvents object to the store
+ * @param {object[]} state - array of event objects in state
+ * @param {string} action - action to update setEvents array
+ */
+
 export default function (state = [], action) {
   switch (action.type) {
     case SET_EVENTS:
