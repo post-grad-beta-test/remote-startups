0cb419ea4db9834c136f784bf02a75636618a4e9
Emma Slight <emma.slight@icloud.com>
2020-12-07 17:37:58 +1300

    change createStore to configureStore

--- client/store.js
+++ client/store.js
@@ -1,9 +1,9 @@
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import reducers from './reducers'
 
-const store = createStore(reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const store = configureStore({
+  reducer: reducers
+})
 
 export default store
