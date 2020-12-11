a67b71e5118cafc6e17d8afeef0550e9da6c6a59
Emma Slight <emma.slight@icloud.com>
2020-12-11 14:26:43 +1300

    add disable join button function to Button prop

--- client/components/eventCard.jsx
+++ client/components/eventCard.jsx
@@ -11,11 +11,9 @@ import {
   setDisabled,
 } from '../actions'
 
-const EventCard = (user, events) => {
+const EventCard = ({ user, events, joinedIds }) => {
   const [listEvents, setListEvents] = useState([])
   const dispatch = useDispatch()
-  const [isBtnDisabled, setBtnDisabled] = useState(false)
-  console.log(isBtnDisabled)
 
   /**
    * a function that dispatches a request for the user to join an event
@@ -25,7 +23,6 @@ const EventCard = (user, events) => {
   const subscribe = (userId, eventId) => {
     // dispatch(setDisabled(eventId)
     dispatch(attendEvent(userId, eventId))
-      .then(setBtnDisabled(true))
       .then(dispatch(fetchEventIds(userId)))
       .then((eventIds) => console.log(eventIds))
   }
@@ -34,9 +31,9 @@ const EventCard = (user, events) => {
     dispatch(loadAllEvents()).then(setListEvents)
   }, [])
 
-  useEffect(() => {
-    setBtnDisabled
-  })
+  // useEffect(() => {
+  //   setBtnDisabled
+  // })
   // useEffect(() => {
   //   setBtnDisabled(events.id.disabled)
   // }, {})
@@ -45,98 +42,105 @@ const EventCard = (user, events) => {
   return (
     <div className='container'>
       <Grid gap='medium' columns={{ count: 'fit', size: 'medium' }}>
-        {listEvents.map((event) => (
-          <div key={event.id}>
-            <Box
-              align='center'
-              pad='small'
-              background={{
-                // 0: 'b',
-                // 1: 'r',
-                // 2: 'a',
-                // 3: 'n',
-                // 4: 'd',
-                color: 'white',
-                image: "url('')",
-                position: 'bottom',
-              }}
-              round='medium'
-              margin='medium'
-              direction='column'
-              alignSelf='center'
-              animation={{ type: 'fadeIn', size: 'medium' }}
-              elevation='medium'
-            >
-              <Box
-                align='start'
-                justify='start'
-                pad='small'
-                direction='row'
-                alignSelf='start'
-              />
+        {listEvents.map((event) => {
+          console.log('event map', event, joinedIds)
+          return (
+            <div key={event.id}>
               <Box
                 align='center'
-                justify='center'
-                pad='xsmall'
-                margin='xsmall'
-                animation='zoomIn'
-                hoverIndicator
-                gap='xsmall'
+                pad='small'
+                background={{
+                  // 0: 'b',
+                  // 1: 'r',
+                  // 2: 'a',
+                  // 3: 'n',
+                  // 4: 'd',
+                  color: 'white',
+                  image: "url('')",
+                  position: 'bottom',
+                }}
+                round='medium'
+                margin='medium'
+                direction='column'
+                alignSelf='center'
+                animation={{ type: 'fadeIn', size: 'medium' }}
+                elevation='medium'
               >
-                <AnIcon size='xlarge' />
-
-                <Heading
-                  level='2'
-                  size='medium'
-                  margin='xsmall'
-                  textAlign='center'
-                  truncate={false}
-                >
-                  {event.name}
-                </Heading>
-                {/* <Image src={`project.images/project_image_${imageNum}.svg`} opacity="strong" /> */}
-                <Text weight='bold' textAlign='center' size='medium'>
-                  Dates: {event.date_start} - {event.date_end}
-                </Text>
-                <Paragraph size='small' margin='medium' textAlign='center'>
-                  {event.description}
-                </Paragraph>
-
+                <Box
+                  align='start'
+                  justify='start'
+                  pad='small'
+                  direction='row'
+                  alignSelf='start'
+                />
                 <Box
                   align='center'
                   justify='center'
-                  pad='small'
-                  direction='row-responsive'
-                  flex
-                  alignSelf='center'
-                  basis='xxsmall'
-                  gap='small'
+                  pad='xsmall'
                   margin='xsmall'
+                  animation='zoomIn'
+                  hoverIndicator
+                  gap='xsmall'
                 >
-                  <Button
-                    disabled={isBtnDisabled}
-                    label='Join'
-                    icon={<Add />}
-                    onClick={() => subscribe(user.user, event.id)}
-                  />
-                  {/* {!isLoading && (
+                  <AnIcon size='xlarge' />
+
+                  <Heading
+                    level='2'
+                    size='medium'
+                    margin='xsmall'
+                    textAlign='center'
+                    truncate={false}
+                  >
+                    {event.name}
+                  </Heading>
+                  {/* <Image src={`project.images/project_image_${imageNum}.svg`} opacity="strong" /> */}
+                  <Text weight='bold' textAlign='center' size='medium'>
+                    Dates: {event.date_start} - {event.date_end}
+                  </Text>
+                  <Paragraph size='small' margin='medium' textAlign='center'>
+                    {event.description}
+                  </Paragraph>
+
+                  <Box
+                    align='center'
+                    justify='center'
+                    pad='small'
+                    direction='row-responsive'
+                    flex
+                    alignSelf='center'
+                    basis='xxsmall'
+                    gap='small'
+                    margin='xsmall'
+                  >
+                    <Button
+                      disabled={Boolean(
+                        joinedIds.find((item) => item.project_id === event.id)
+                      )}
+                      label='Join'
+                      icon={<Add />}
+                      onClick={() => subscribe(user, event.id)}
+                    />
+                    {/* {!isLoading && (
                     <Button label='Join' icon={<Add />} onClick={subscribe} />
                   )}
                   {isLoading && <PowerCycle label='Joining now...' />} */}
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-          </div>
-        ))}
+            </div>
+          )
+        })}
       </Grid>
     </div>
   )
 }
 
 function mapStateToProps(state) {
+  console.log(state)
   return {
     events: state.setEvents,
     user: state.createUser.id,
+    joinedIds: state.setJoinEvent,
   }
 }
 export default connect(mapStateToProps)(EventCard)
