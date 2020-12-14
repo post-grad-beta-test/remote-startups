0f13b1cbe3fbfc00cf943a64d87a5fad9c3a82dd
Lex <lexx_stokes@live.com>
2020-12-14 15:39:14 +1300

    remove auth.

---
+++ server/Db/connection.js
@@ -0,0 +1,7 @@
+const knex = require('knex')
+//is this changed
+const config = require('../../knexfile')
+const env = process.env.NODE_ENV || 'development'
+const connection = knex(config[env])
+
+module.exports = connection
