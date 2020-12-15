145bffcf46d747b26d873ab20e6defc97317b187
Emma <60733989+emma-r-slight@users.noreply.github.com>
2020-12-15 16:25:45 +1300

    Update client/reducers/setEvents.js
    Co-authored-by: Phoenix <todofixthis@users.noreply.github.com>

--- client/reducers/setEvents.js
+++ client/reducers/setEvents.js
@@ -4,7 +4,7 @@ import { SET_EVENTS } from '../actions'
  * setEvents reducer the adds the setEvents object to the store
  * @param {object[]} state - current array of events in state
  * @param {string} action - action to update events array
- * @returns {array}  updated state
+ * @returns {array} updated state
  */
 
 export default function (state = [], action) {
